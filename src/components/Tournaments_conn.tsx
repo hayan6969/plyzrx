3557725015489489d2f67da242edf64853996599
sharyar <sharyar430@gmail.com>
2025-02-25 00:56:23 +0500

    landing page fixation

--- src/components/Tournaments_conn.tsx
+++ src/components/Tournaments_conn.tsx
@@ -4,7 +4,6 @@ import Tourncard from "./Tourncard";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import { Pagination } from "swiper/modules";
 
 const tournaments = [
   {
