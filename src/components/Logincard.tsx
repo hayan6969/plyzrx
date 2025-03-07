af97c62313fb474229e026e3c1c6c4f326749a54
sharyar <sharyar430@gmail.com>
2025-03-08 00:29:18 +0500

    cant pay without login

--- src/components/Logincard.tsx
+++ src/components/Logincard.tsx
@@ -1,8 +1,5 @@
-'use client';
 
-import { useState } from 'react';
 import { Card, CardContent } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 
 export default function PopupCard() {
