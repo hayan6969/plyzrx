c0f096d0c8fc43cabd8a6b1db1ca87b7db715391
sharyarnaveed <sharyarmalik430@gmail.com>
2025-04-12 15:35:55 +0500

    email template changed

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,14 +6,14 @@ import { cookies } from "next/headers";
 const blockedStates = ['AR', 'CT', 'DE', 'LA', 'SD', 'ME', 'IN', 'NJ']
 
 export async function middleware(request: NextRequest) {
-  const ip = request.headers.get("x-forwarded-for")?.split(",")[0] || "8.8.8.8";
+  // const ip = request.headers.get("x-forwarded-for")?.split(",")[0] || "8.8.8.8";
 
-  const res = await fetch(`http://ip-api.com/json/${ip}`);
-  const geo = await res.json();
-  console.log(geo);
-  if (geo.country === 'US' && blockedStates.includes(geo.region)) {
-    return NextResponse.redirect(new URL('/access-denied', request.url));
-  }
+  // const res = await fetch(`http://ip-api.com/json/${ip}`);
+  // const geo = await res.json();
+  // console.log(geo);
+  // if (geo.country === 'US' && blockedStates.includes(geo.region)) {
+  //   return NextResponse.redirect(new URL('/access-denied', request.url));
+  // }
 
 
   const cookiesStore = cookies();
