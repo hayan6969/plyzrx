5ba5a85cd10f8688819ad98b21839ff22f420744
sharyarnaveed <sharyarmalik430@gmail.com>
2025-04-12 16:25:08 +0500

    changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,11 +6,16 @@ import { cookies } from "next/headers";
 // const blockedStates = ['AR', 'CT', 'DE', 'LA', 'SD', 'ME', 'IN', 'NJ']
 
 export async function middleware(request: NextRequest) {
+  const ip = request.headers.get("x-forwarded-for")?.split(",")[0] || "8.8.8.8";
 
-  try {
-   
+  const res = await fetch(`http://ip-api.com/json/${ip}`);
+  const geo = await res.json();
+  console.log(geo);
+  if (geo.country === 'Pakistan') {
+    return NextResponse.redirect(new URL('/access-denied', request.url));
+  }
 
-   
+
   const cookiesStore = cookies();
   const pathname = request.nextUrl.pathname;
 
@@ -23,14 +28,6 @@ export async function middleware(request: NextRequest) {
       return NextResponse.redirect(new URL("/login", request.url));
     }
   }
-
-
-  } catch (error) {
-    console.error('Middleware error:', error);
-    return NextResponse.next();
-  }
-
-
 }
 
 // Middleware Configuration
